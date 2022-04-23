d7b8578ae461d14ad9e5db32350b70f2baff696a
Berktug Ozkan <b.kaan.ozkan@gmail.com>
2022-04-23 15:45:48 +0300

    config: central-unit: initialized

---
+++ CentralUnit/src/main/java/io/github/spaceymonk/khb/centralunit/CentralUnitApplication.java
@@ -0,0 +1,13 @@
+package io.github.spaceymonk.khb.centralunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralUnitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralUnitApplication.class, args);
+    }
+
+}
---
+++ CentralUnit/src/test/java/io/github/spaceymonk/khb/centralunit/CentralUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.spaceymonk.khb.centralunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralUnitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
