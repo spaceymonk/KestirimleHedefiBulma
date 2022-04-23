0001554315b2fa509fd533f5cd467efafb541433
spaceymonk <b.kaan.ozkan@gmail.com>
2022-04-23 22:17:48 +0300

    config: sensor-app: initialization

---
+++ SensorApp/src/test/java/io/github/spaceymonk/khb/sensorapp/SensorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.spaceymonk.khb.sensorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
